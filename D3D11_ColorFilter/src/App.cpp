fb715afc4718cbb657b5fb2f0885304b1967f55e
Pocol <project.asura@gmail.com>
2020-07-31 23:59:19 +0900

    カラー行列処理を追加。

--- D3D11_ColorFilter/src/App.cpp
+++ D3D11_ColorFilter/src/App.cpp
@@ -28,6 +28,141 @@ struct alignas(16) CbColorFilter
     asdx::Matrix    ColorMatrix;
 };
 
+asdx::Matrix CreateBrightnessMatrix(float brightness)
+{
+    return asdx::Matrix::CreateScale(brightness);
+}
+
+asdx::Matrix CreateSaturationMatrix(float r, float g, float b)
+{
+    // https://docs.microsoft.com/ja-jp/windows/win32/direct2d/saturation
+    return asdx::Matrix(
+        0.213f + 0.787f * r,
+        0.213f - 0.213f * r,
+        0.213f - 0.213f * r,
+        0.0f,
+
+        0.715f - 0.715f * g,
+        0.715f + 0.285f * g,
+        0.715f - 0.715f * g,
+        0.0f,
+
+        0.072f - 0.072f * b,
+        0.072f - 0.072f * b,
+        0.072f + 0.928f * b,
+        0.0f,
+
+        0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+asdx::Matrix CreateSaturationMatrix(float saturation)
+{
+    return CreateSaturationMatrix(saturation, saturation, saturation);
+}
+
+asdx::Matrix CreateContrastMatrix(float contrast)
+{
+    const auto t = (1.0f - contrast) * 0.5f;
+    return asdx::Matrix(
+        contrast, 0.0f, 0.0f, 0.0f,
+        0.0f, contrast, 0.0f, 0.0f,
+        0.0f, 0.0f, contrast, 0.0f,
+        t, t, t, 1.0f);
+}
+
+asdx::Matrix CreateHueMatrix(float hue)
+{
+    // https://docs.microsoft.com/ja-jp/windows/win32/direct2d/hue-rotate
+    auto rad = asdx::ToRadian(hue);
+    auto u = cosf(rad);
+    auto w = sinf(rad);
+
+    return asdx::Matrix(
+        0.213f + 0.787f * u - 0.213f * w,
+        0.213f - 0.213f * u + 0.143f * w,
+        0.213f - 0.213f * u - 0.787f * w,
+        0.0f,
+
+        0.715f - 0.715f * u - 0.715f * w,
+        0.715f + 0.285f * u + 0.140f * w,
+        0.715f - 0.715f * u - 0.283f * w,
+        0.0f,
+
+        0.072f - 0.072f * u + 0.928f * w,
+        0.072f - 0.072f * u - 0.283f * w,
+        0.072f + 0.928f * u + 0.072f * w,
+        0.0f,
+
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f);
+}
+
+asdx::Matrix CreateSepiaMatrix(float tone)
+{
+    const asdx::Vector3 W(0.298912f, 0.586611f, 0.114478f);
+    const asdx::Vector3 Sepia(0.941f, 0.784f, 0.569f);
+
+    return asdx::Matrix(
+        tone * W.x * Sepia.x + (1.0f - tone),
+        tone * W.x * Sepia.y,
+        tone * W.x * Sepia.z,
+        0.0f,
+
+        tone * W.y * Sepia.x,
+        tone * W.y * Sepia.y + (1.0f - tone),
+        tone * W.y * Sepia.z,
+        0.0f,
+
+        tone * W.z * Sepia.x,
+        tone * W.z * Sepia.y,
+        tone * W.z * Sepia.z + (1.0f - tone),
+        0.0f,
+
+        0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+asdx::Matrix CreateGrayScaleMatrix(float tone)
+{
+    const asdx::Vector3 GrayScale(0.22015f, 0.706655f, 0.071330f);
+    return asdx::Matrix(
+        tone * GrayScale.x + (1.0f - tone),
+        tone * GrayScale.x,
+        tone * GrayScale.x,
+        0.0f,
+
+        tone * GrayScale.y,
+        tone * GrayScale.y + (1.0f - tone),
+        tone * GrayScale.y,
+        0.0f,
+
+        tone * GrayScale.z,
+        tone * GrayScale.z,
+        tone * GrayScale.z + (1.0f - tone),
+        0.0f,
+
+        0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+asdx::Matrix CreateNegaposiMatrix()
+{
+    return asdx::Matrix(
+        -1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, -1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, -1.0f, 0.0f,
+        1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+asdx::Matrix CreateSwapRandG()
+{
+    return asdx::Matrix(
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f);
+}
+
 } // namespace 
 
 
@@ -209,7 +344,7 @@ void App::OnFrameRender(asdx::FrameEventArgs& param)
         CbColorFilter res = {};
         res.ThreadX = x;
         res.ThreadY = y;
-        res.ColorMatrix = asdx::Matrix::CreateIdentity();
+        res.ColorMatrix = CreateSepiaMatrix(0.85f);
 
         m_pDeviceContext->UpdateSubresource(pCB, 0, nullptr, &res, 0, 0);
 
